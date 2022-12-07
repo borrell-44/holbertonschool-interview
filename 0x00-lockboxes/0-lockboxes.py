688675a7d2066ef99aaff470175b60675e405f3c
borrell-44 <borrell-44.alexanderborrell44@hotmail.com>
2022-12-07 07:07:39 -0400

    Documenting files

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+"""You have n number of locked boxes in front of you. Each 
+box is numbered sequentially from 0 to n - 1 and each 
+box may contain keys to the other boxes.""" 
+
 def canUnlockAll(boxes):
+    """Determines if all boxes can be open"""
+
     condition = False
 
     if len(boxes) < 2:
