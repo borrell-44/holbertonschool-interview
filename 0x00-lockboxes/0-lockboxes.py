5fc34c63a45254008c7fe06b3670390e8865e8c8
borrell-44 <borrell-44.alexanderborrell44@hotmail.com>
2022-12-07 09:47:26 -0400

    Hot Fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,22 +7,22 @@ box may contain keys to the other boxes."""
 def canUnlockAll(boxes):
     """Determines if all boxes can be open"""
 
-    #condition = False
+    condition = False
 
-    for i in range(0, len(boxes) - 1):
+    """for i in range(0, len(boxes) - 1):
         if len(boxes[i]) < 1:
-            return True
-    return True
+            return False
+    return True"""
 
-    """if len(boxes) <= 2:
+    if len(boxes) <= 2:
         return True
 
     for i in range(0, len(boxes) - 1):
         #print(boxes[i])
-        for l in range(0, len(boxes)):
+        for l in range(0, len(boxes) - 1):
             if (i + 1) in boxes[l] and len(boxes[i]) > 0:
                 condition = True
         if condition is False:
             return False
         condition = False
-    return True"""
+    return True
