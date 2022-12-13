b6467e8ecead658ccd6bee99c9f9164965c02922
JP <alexanderborrell44@hotmail.com>
2022-12-13 10:05:59 -0400

    Committing task 0

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number to a sorted singly linked list
+ * 
+ * @head: first node of the list
+ * @number: value for the new node
+ * 
+ * Return: address of nre node, or NULL if failed
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+    new->n = number;
+
+    if (*head == NULL)
+    {
+        new->next = NULL;
+        *head = new;
+    }
+
+    if (current->n > number)
+    {
+        new->next = current;
+        *head = new;
+        return (new);
+    }
+
+    while (current->next != NULL)
+    {
+        if (current->next->n > number)
+        {
+            new->next = current->next;
+            current->next = new;
+            return (new);
+        }
+        current = current->next;
+    }
+
+    current->next = new;
+    new->next = NULL;
+
+    return (new);
+}
